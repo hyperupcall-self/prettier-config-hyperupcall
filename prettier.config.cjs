4d998e8452e847981580abb3a7e9fc8bb295ae2c
Edwin Kofler <edwin@kofler.dev>
2023-07-29 04:01:36 -0700

    Make main entrypoint JavaScript to avoid import assertion errors

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+exports.default = {
+	printWidth: 90,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: "all", // Default changed to "all" in v2.0.0
+	arrowParens: "always", // Default changed to "always" in v2.0.0
+	endOfLine: "lf", // Default changed to "lf" in v2.0.0
+};
