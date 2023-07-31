8c3a9fb35d4a2a3372f82f5c9ec656029680b2eb
Edwin Kofler <edwin@kofler.dev>
2023-07-31 16:48:13 -0700

    Update version comments

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
 	semi: false,
 	singleQuote: true,
 	jsxSingleQuote: true,
-	trailingComma: "all", // Default changed to "all" in v2.0.0
-	arrowParens: "always", // Default changed to "always" in v2.0.0
-	endOfLine: "lf", // Default changed to "lf" in v2.0.0
-};
+	trailingComma: 'all', // Default changed from "es5" to "all" in v3.0.0
+	arrowParens: 'always', // Default changed from "avoid" to "always" in v2.0.0
+	endOfLine: 'lf', // Default changed from "auto" to "lf" in v2.0.0
+}
